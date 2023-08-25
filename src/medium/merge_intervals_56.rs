1a9a57c4b98a524b9eed35e35ac420fb4575c644
levin li <3259963@qq.com>
2023-08-25 22:43:52 +0800

    56.V2

--- src/medium/merge_intervals_56.rs
+++ src/medium/merge_intervals_56.rs
@@ -2,16 +2,20 @@ pub struct Solution;
 
 impl Solution {
     pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        use std::cmp::max;
         let mut result: Vec<Vec<i32>> = Vec::new();
 
-        let mut intervals = intervals.clone();
-        intervals.sort_by(|a, b| b[0].cmp(&a[0]));
+        // let mut _intervals = intervals.clone();
+        let mut _intervals = Vec::with_capacity(intervals.len());
+        _intervals.extend(&intervals);
 
-        while let Some(current) = intervals.pop() {
+        _intervals.sort_by(|&a, &b| b[0].cmp(&a[0]));
+
+        while let Some(current) = _intervals.pop() {
             if let Some(prev) = result.last_mut() {
-                if current[0] <= prev[1] && prev[1] < current[1] {
-                    prev[1] = current[1];
-                } else if prev[1] < current[0] {
+                if prev[1] >= current[0] {
+                    prev[1] = max(prev[1], current[1]);
+                } else {
                     result.push(current.clone());
                 }
             } else {
--- tests/merge_intervals_56.rs
+++ tests/merge_intervals_56.rs
@@ -27,3 +27,35 @@ fn test_all_merge() {
     let expect: Vec<Vec<i32>> = vec![vec![1, 16]];
     assert_eq!(result, expect);
 }
+
+#[test]
+fn test_example_1() {
+    let mut input = Vec::new();
+    input.push(vec![1, 3]);
+    input.push(vec![2, 6]);
+    input.push(vec![8, 10]);
+    input.push(vec![15, 18]);
+
+    let mut output = Vec::new();
+    output.push(vec![1, 6]);
+    output.push(vec![8, 10]);
+    output.push(vec![15, 18]);
+
+    let result = Solution::merge(input);
+
+    assert_eq!(result, output);
+}
+
+#[test]
+fn test_example_2() {
+    let mut input = Vec::new();
+    input.push(vec![1, 4]);
+    input.push(vec![4, 5]);
+
+    let mut output = Vec::new();
+    output.push(vec![1, 5]);
+
+    let result = Solution::merge(input);
+
+    assert_eq!(result, output);
+}
