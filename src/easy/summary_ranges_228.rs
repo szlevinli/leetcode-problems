8fa45589198d0b44f56331dfe9ebbce0c969bf63
levin li <3259963@qq.com>
2023-08-26 15:23:25 +0800

    complete 228

--- src/easy/summary_ranges_228.rs
+++ src/easy/summary_ranges_228.rs
@@ -2,6 +2,29 @@ pub struct Solution;
 
 impl Solution {
     pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
-        unimplemented!()
+        fn to_string(low: i32, high: i32) -> String {
+            if low == high {
+                format!("{low}")
+            } else {
+                format!("{low}->{high}")
+            }
+        }
+
+        let len = nums.len();
+        let mut low = 0;
+        let mut ans = Vec::new();
+
+        while low < len {
+            let mut high = low;
+
+            while high + 1 < len && nums[high] == nums[high + 1] - 1 {
+                high += 1;
+            }
+
+            ans.push(to_string(nums[low], nums[high]));
+            low = high + 1;
+        }
+
+        ans
     }
 }
