a4a8a4c29324d4eaa6526ee157c8bb81953c6009
levin li <3259963@qq.com>
2023-08-28 21:38:37 +0800

    start `57. insert interval`

--- src/medium.rs
+++ src/medium.rs
@@ -1,2 +1,3 @@
+pub mod insert_interval_57;
 pub mod merge_intervals_56;
 pub mod zig_zag_conversion_6;
---
+++ src/medium/insert_interval_57.rs
@@ -0,0 +1,7 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+        unimplemented!()
+    }
+}
